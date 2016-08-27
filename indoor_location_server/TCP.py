75091fad7059fe2f961a4dd0862b09c95001ced8
sherlockbear <123725390@qq.com>
2016-08-27 12:51:24 +0800

    加入多线程；重新修改消息格式（提取坐标）

--- indoor_location_server/TCP.py
+++ indoor_location_server/TCP.py
@@ -1,11 +1,13 @@
-from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH)    #可以通过as起别名
+from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH,ThreadingMixIn as TMI)    #可以通过as起别名
 from time import ctime
 from extract_fingerprint import extract
 from insert import *
 
-HOST = '192.168.1.100'
+HOST = '192.168.1.105'
 PORT = 1234
 ADDR = (HOST, PORT)
+class Server(TMI, TCP):                                         #变动位置  
+    pass
 
 class MyRequestHandler(SRH):
     def handle(self):
@@ -13,13 +15,14 @@ class MyRequestHandler(SRH):
         self.wfile.write(('[%s] %s' % (ctime(), self.rfile.readline().decode("UTF-8"))).encode("UTF-8"))
         request = str(self.request.recv(1024), encoding = "utf-8")
         print(request)
+        self.request.sendall(bytes('OK',encoding = "utf-8"))
         #插入数据
         '''fingerprint=extract(request)
         print(fingerprint)
         storageOfFingerprint([1,5,8],fingerprint)
         data_view()'''
 
-tcpServ = TCP(ADDR, MyRequestHandler) 
+tcpServ = Server(ADDR, MyRequestHandler) 
 print ('等待新的连接。。。。')
 
 tcpServ.serve_forever()
--- indoor_location_server/extract_fingerprint.py
+++ indoor_location_server/extract_fingerprint.py
@@ -1,7 +1,11 @@
 def extract(req):
     lis = []
-    for i in req.split('+CWLAP:')[1:]:
+    coord = []
+    sp = req.split('+CWLAP:')
+    coord.append(int(sp[0].split(',')[0][-2:]))
+    coord.append(int(sp[0].split(',')[1][0:2]))
+    for i in sp[1:]:
         c = i.split(',')
         lis.append(c[3][1:-1])
         lis.append(int(c[2]))
-    return lis
+    return coord,lis
