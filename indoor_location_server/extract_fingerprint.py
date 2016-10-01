eb1c71fa4acf43f172d9913d0135f65475562dbc
sherlockbear <123725390@qq.com>
2016-10-01 12:49:53 +0800

    Revert "TCP中加入信号强度排序"
    This reverts commit f8ece805dfc6dc4635f3f0243480bc3d251e8f1d.

--- indoor_location_server/extract_fingerprint.py
+++ indoor_location_server/extract_fingerprint.py
@@ -1,5 +1,4 @@
 def extract(req):
-    dic = {}
     lis = []
     coord = []
     sp = req.split('+CWLAP:')
@@ -7,7 +6,6 @@ def extract(req):
     coord.append(int(sp[0].split(',')[1][0:2]))
     for i in sp[1:]:
         c = i.split(',')
-        dic[c[3][1:-1]]=int(c[2])
-    for each_items in sorted(dic.items(),key=lambda d:d[1],reverse = True ):
-        lis.extend(list(each_items))
+        lis.append(c[3][1:-1])
+        lis.append(int(c[2]))
     return coord,lis
