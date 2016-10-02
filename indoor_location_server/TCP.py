cd0a5eb336f847191dfeb880af4a0f1b238cbeee
sherlockbear <123725390@qq.com>
2016-10-02 18:15:54 +0800

    第二版

--- indoor_location_server/TCP.py
+++ indoor_location_server/TCP.py
@@ -1,9 +1,9 @@
-from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH,ThreadingMixIn as TMI)    #可以通过as起别名
+from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH, ThreadingMixIn as TMI)  #可以通过as起别名
 from time import ctime
 from extract_fingerprint import extract
 from insert import *
 
-HOST = '192.168.1.105'
+HOST = '192.168.1.101'
 PORT = 1234
 ADDR = (HOST, PORT)
 class Server(TMI, TCP):
@@ -12,10 +12,16 @@ class Server(TMI, TCP):
 class MyRequestHandler(SRH):
     def handle(self):
         print ('已经连接:', self.client_address)
-        self.wfile.write(('[%s] %s' % (ctime(), self.rfile.readline().decode("UTF-8"))).encode("UTF-8"))
-        request = str(self.request.recv(1024), encoding = "utf-8")
-        print(request)
-        self.request.sendall(bytes('OK',encoding = "utf-8"))
+        while True:  
+            request = self.request.recv(1024)  
+            if not request:   
+                break
+            request_pri = str(request, encoding = "utf-8")
+            print(request_pri)
+            '''fingerprint=extract(request_pri)
+            print(fingerprint)'''
+            self.request.send(request) 
+        '''self.request.sendall(bytes('OK',encoding = "utf-8"))'''
         #插入数据
         '''fingerprint=extract(request)
         print(fingerprint)
---
+++ indoor_location_server/test/suoduanbuchang.py
@@ -0,0 +1,15 @@
+str1=''
+a=[]
+f=open("wifi3.txt","r")
+for each_line in f.readlines():
+    f1=open('wifi4.txt','a')
+    if each==[*,*]:
+        f1.write(each_line+'\n')
+    else:
+        
+    f1.close
+f1=open('wifi4.txt','a')
+print(extract(str1))
+f1.write(str(extract(str1))+'\n')
+f1.close
+f.close
