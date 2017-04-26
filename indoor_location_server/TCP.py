15f8ca79e0f2e343615611eeefdbf719cbfd4846
sherlockbear <123725390@qq.com>
2017-04-26 21:17:45 +0800

    2017-4-26

--- indoor_location_server/TCP.py
+++ indoor_location_server/TCP.py
@@ -2,31 +2,39 @@ from socketserver import (TCPServer as TCP, StreamRequestHandler as SRH, Threadi
 from time import ctime
 from extract_fingerprint import extract
 from insert import *
-
+import threading
+import os
+ 
 HOST = '192.168.1.101'
 PORT = 1234
 ADDR = (HOST, PORT)
+allrequest = threading.local()
 class Server(TMI, TCP):
     pass
 
 class MyRequestHandler(SRH):
     def handle(self):
-        print ('已经连接:', self.client_address)
-        while True:  
-            request = self.request.recv(1024)  
-            if not request:   
-                break
-            request_pri = str(request, encoding = "utf-8")
-            print(request_pri)
-            '''fingerprint=extract(request_pri)
-            print(fingerprint)'''
-            self.request.send(request) 
-        '''self.request.sendall(bytes('OK',encoding = "utf-8"))'''
+        #pid = os.fork()
+        #if pid == 0:
+            #print(os.getpid())
+        #else:
+            print ('已经连接:', self.client_address)
+            request_pri = ''
+            while True:
+                allrequest.request = self.request.recv(1024)
+                if not allrequest.request:
+                    break
+                request_pri = request_pri+(str(allrequest.request, encoding = "utf-8"))
+                print(request_pri)
+            fingerprint=extract(request_pri)
+            print(fingerprint)
+                #self.request.send(request)
+        #'''self.request.sendall(bytes('OK',encoding = "utf-8"))'''
         #插入数据
-        '''fingerprint=extract(request)
-        print(fingerprint)
-        storageOfFingerprint([1,5,8],fingerprint)
-        data_view()'''
+        #'''fingerprint=extract(request)
+        #print(fingerprint)
+        #storageOfFingerprint([1,5,8],fingerprint)
+        #data_view()'''
 
 tcpServ = Server(ADDR, MyRequestHandler)
 print ('等待新的连接。。。。')
