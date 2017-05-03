50a47928f47b9f0639b0202c7ffeafef71e889e1
sherlockbear <123725390@qq.com>
2017-05-03 23:17:10 +0800

    规范格式

--- indoor_location_server/test/extract_fingerprint.py
+++ indoor_location_server/test/extract_fingerprint.py
@@ -1,32 +1,33 @@
 def extract(req):
     dic = {}
-    lis=[]
+    lis = []
     for i in req.split('+CWLAP:')[1:]:
         c = i.split(',')
-        dic[c[3][1:-1]]=int(c[2])
-    for each_items in sorted(dic.items(),key=lambda d:d[1],reverse = True ):
+        dic[c[3][1:-1]] = int(c[2])
+    for each_items in sorted(dic.items(), key=lambda d: d[1], reverse=True):
         lis.extend(list(each_items))
     return lis
 
-str1=''
-a=[]
-f=open("wifi.txt","r")
+
+str1 = ''
+a = []
+f = open("wifi.txt", "r")
 for each_line in f.readlines():
-    f1=open('wifi5.txt','a')
-    if each_line[0]=='$':
+    f1 = open('wifi5.txt', 'a')
+    if each_line[0] == '$':
         print(extract(str1))
-        f1.write(str(extract(str1))+'\n')
-        a.append(int(each_line.split(',')[0][-1]))
+        f1.write(str(extract(str1)) + '\n')
+        a.append(int(each_line.split(',')[0][-2:]))
         a.append(int(each_line.split(',')[1][0:2]))
         print(a)
-        f1.write(str(a)+'\n')
-        str1=''
-        a=[]
+        f1.write(str(a) + '\n')
+        str1 = ''
+        a = []
     else:
         str1 = str1 + each_line
     f1.close
-f1=open('wifi5.txt','a')
+f1 = open('wifi5.txt', 'a')
 print(extract(str1))
-f1.write(str(extract(str1))+'\n')
+f1.write(str(extract(str1)) + '\n')
 f1.close
 f.close
